868285d8e26a3e7ad5095303f3c0bbadc8b34e44
herdiosaputra01 <herdiosaputra06@gmail.com>
2025-05-17 23:38:02 +0700

    buat baru

--- laravelis62/resources/views/Dosen/form.blade.php
+++ laravelis62/resources/views/Dosen/form.blade.php
@@ -2,36 +2,49 @@
 
 @section('content')
 <div class="container">
-    <div class="row mt-5">
-        <div class="col-6 m-auto">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
             <div class="card">
-                <div class="card-header bg-dark-subtle">
-                    <h3 class="fw-bold text-center">{{ __('FORM DOSEN') }}</h3>
-                </div>
+                <div class="card-header">Form Tambah Data</div>
+
                 <div class="card-body">
-                    <form action="{{ route('dosen.store') }}" method="POST">
+                    <form method="post" action="/dosen" enctype="multipart/form-data">
                         @csrf
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label"> NIDN </label>
+                            <label for="exampleInputEmail1" class="form-label">NIDN</label>
                             <input type="text" name="nidn" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                         </div>
-
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label"> Nama Dosen</label>
+                            <label for="exampleInputPassword1" class="form-label">Nama Lengkap</label>
                             <input type="text" name="nama" class="form-control" id="exampleInputPassword1">
                         </div>
-
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label"> Email </label>
-                            <input type="text" name="email" class="form-control" id="exampleInputPassword1">
+                            <label for="exampleInputPassword1" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="exampleInputPassword1">
                         </div>
-                        <button href="" type="submit" class="btn btn-primary">Submit</button>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Rumpun</label>
+                            <select name="rumpun" id="" class="form-control">
+                                <option value="">-Pilih Rumpun-</option>
+                                <option value="Komputer">Komputer</option>
+                                <option value="Bisnis">Bisnis</option>
+                                <option value="Sekretaris">Sekretaris</option>
+                                <option value="Akuntansi">Akuntansi</option>
+                                <option value="Manajemen">Manajemen</option>
+                                <option value="Wirausaha">Wirausaha</option>
+                                <option value="Marketing">Marketing</option>
+                            </select>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">No Handphone</label>
+                            <input type="nohp" name="nohp" class="form-control" id="exampleInputPassword1">
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Tambah</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-
 @endsection
--- laravelis62/resources/views/Dosen/index.blade.php
+++ laravelis62/resources/views/Dosen/index.blade.php
@@ -1,87 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-
-@if (session('success'))
-    <div class="modal fade" id="successModal" tabindex="-1" aria-labelledby="successModalLabel" aria-hidden="false">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-body">
-                    {{ session('success') }}<i class="fa-solid fa-check"></i>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
-                </div>
-            </div>
-        </div>
-    </div>
-@endif
-
 <div class="container">
-    <div class="row mt-5">
-        <div class="col-8 m-auto">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
             <div class="card">
-                <div class="card-header bg-dark-subtle">
-                    <h3 class="float-start fw-bold">{{ __('DATA DOSEN') }}</h3>
-                    <span class="float-end btn btn-primary">
-                        <a class="text-light" href="dosen/add"> <i class="fa-solid fa-user-plus"></i> Add </a>
-                    </span>
-                </div>
+                <div class="card-header"><a href="/dosen/tambah" class="btn btn-primary btn-sm">Tambah Data</a></div>
+
                 <div class="card-body">
+
                     <table class="table">
                         <thead>
-                          <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">NIDN</th>
-                            <th scope="col">NAMA</th>
-                            <th scope="col">EMAIL</th>
-                          </tr>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">NIDN</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Action</th>
+                            </tr>
                         </thead>
                         <tbody>
-                          <tr>
-                            <th scope="row">1</th>
-                            <td>1937146</td>
-                            <td>Maulian Saputra</td>
-                            <td>mauliansaputra@gmail.com</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">2</th>
-                            <td>9714082</td>
-                            <td>Hadi Saputra</td>
-                            <td>hadisaputra@gmail.com</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">3</th>
-                            <td>018624139</td>
-                            <td>Ismanuddin</td>
-                            <td>ismanuddin@gmail.com</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">4</th>
-                            <td>08235624139</td>
-                            <td>Susilawati Yahya</td>
-                            <td>susliwatiyahya@gmail.com</td>
-                          </tr>
-                          <tr>
-                            <th scope="row">5</th>
-                            <td>096624139</td>
-                            <td>Rita Warni</td>
-                            <td>ritawarni@gmail.com</td>
-                          </tr>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                <td>
+                                    <a href="" class="btn btn-warning btn-sm">detail</a>
+                                    <a href="" class="btn btn-info btn-sm">edit</a>
+                                    <a href="" class="btn btn-danger btn-sm">hapus</a>
+                                </td>
+                            </tr>
+
                         </tbody>
-                      </table>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
 </div>
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        var successModal = new bootstrap.Modal(document.getElementById('successModal'));
-        successModal.show();
-    });
-</script>
-
-
-
 @endsection
